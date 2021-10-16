473d91029a6bd850ca751b27aba0a2ad3d71bfe4
Monsur Kareem <monsurkareem@gmail.com>
2021-10-16 16:02:01 +0100

    SMS App -project

--- test/services/SchoolServiceImplTest.java
+++ test/services/SchoolServiceImplTest.java
@@ -9,9 +9,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchoolServiceImplTest {
-
-    private SchoolServices service;
     private SchoolDb schoolDb;
+    private SchoolServices service;
 
     @BeforeEach
     void setUp() {
