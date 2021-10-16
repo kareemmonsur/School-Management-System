570196ee263b99ea27e3a92b45c004bb1394d5ba
Monsur Kareem <monsurkareem@gmail.com>
2021-10-16 13:27:44 +0100

    SMS App -project

--- test/services/SchoolServiceImplTest.java
+++ test/services/SchoolServiceImplTest.java
@@ -15,20 +15,22 @@ class SchoolServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        service = new SchoolServiceImpl();
         schoolDb = new SchoolDb();
+        service = new SchoolServiceImpl( schoolDb);
+
     }
 
     @Test
     @DisplayName("Add School")
     void add() {
-        School school1 = new School(
-                "Gidi Grams",
-                "Sport school");
+        School school1 = new School("Gidi Grams", "Sport school");
         assertEquals(1, school1.getId());
-        School school2 = new School("school of logic",
-                "fine school");
-        assertEquals(2, school2.getId());
+        School school2 = new School("school of logic", "fine school");
+       // assertEquals(2, school2.getId());
+        School school3 = new School("school of agric", "fine school");
+       // assertEquals(3, school3.getId());
+        service.addAll(school1, school2, school3);
+        assertEquals(3, schoolDb.getAll().size());
     }
 
     @Test
